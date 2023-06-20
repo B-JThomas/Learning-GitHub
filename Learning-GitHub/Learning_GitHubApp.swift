0a00910d3fa318443223522a02ec2459be9e1372
Bailey <baileythomas02@hotmail.com>
2023-06-20 15:15:23 +1000

    Initial Commit

---
+++ Learning-GitHub/Learning_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GitHubApp.swift
+//  Learning-GitHub
+//
+//  Created by Bailey Thomas on 20/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
